3fa755888b6646d32368372e23d8aea81fa65798
ThiagoAnd <thiago.andrade.it@outlook.com>
2021-09-25 00:28:46 -0300

    Description: .ignore update

---
+++ apache-camel-ws/camel-microservice-a/src/test/java/br/com/thiago/camelmicroservicea/CamelMicroserviceAApplicationTests.java
@@ -0,0 +1,13 @@
+package br.com.thiago.camelmicroservicea;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CamelMicroserviceAApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
